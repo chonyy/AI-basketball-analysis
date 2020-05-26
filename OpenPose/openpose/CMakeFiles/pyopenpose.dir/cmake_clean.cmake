e4c3964feb303e0b6f7c19e2b359438dfafc834e
tcheon8788 <tcheon8788@gmail.com>
2020-05-26 22:51:02 +0800

    fix import in ubuntu

---
+++ OpenPose/openpose/CMakeFiles/pyopenpose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyopenpose.dir/openpose_python.cpp.o"
+  "pyopenpose.cpython-36m-x86_64-linux-gnu.so"
+  "pyopenpose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pyopenpose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenPose/openpose/__init__.py
@@ -0,0 +1,1 @@
+from . import pyopenpose as pyopenpose
